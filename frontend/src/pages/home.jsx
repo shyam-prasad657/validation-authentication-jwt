5a79472d36ff782811f50c330da2b770224e7e27
shyam-prasad657 <shyamsecrets.92@gmail.com>
2025-04-16 21:51:29 +0530

    logout

--- frontend/src/pages/home.jsx
+++ frontend/src/pages/home.jsx
@@ -3,12 +3,17 @@ import { useAuth } from '../hooks/useAuth';
 import { Link } from 'react-router';
 
 const Home = () => {
-        const { auth } = useAuth();
+        const { auth, setAuth } = useAuth();
+        const logout = () => {
+          setAuth({});
+        }
   return (
     <div>
       <h1>Welcome Home {auth?.username}</h1>
       <Link to = '/admin'>go to admin</Link>
       <Link to = '/view'>go to view</Link>
+
+      <button className="btn btn-primary" onClick={logout}>Logout</button>
     </div>
   )
 }
